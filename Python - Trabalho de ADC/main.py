3507adf56f1e06344c23953ac7171cae36ad87cf
marcelo-m7 <117441129+marcelo-m7@users.noreply.github.com>
2022-12-14 13:16:16 +0000

    Removido comentários do código do menu.

--- Python - Trabalho de ADC/main.py
+++ Python - Trabalho de ADC/main.py
@@ -2,11 +2,6 @@ from eventos import (cria_evento,
                      imprime_lista_de_eventos,
                      nome_ficheiro_lista_de_eventos
                      )
-# from utilizadores import (
-#     cria_novo_utilizador,
-#     imprime_lista_de_utilizadores,
-#     nome_ficheiro_lista_de_utilizadores
-# )
 from io_ficheiros import (
     guarda_em_ficheiro,
     le_de_ficheiro
@@ -14,15 +9,12 @@ from io_ficheiros import (
 from io_terminal import (
     imprime_lista_de_dicionarios
 )
-# import time
 
 
 def menu():
     """ main menu da aplicação"""
 
     lista_de_eventos = []
-    lista_de_utilizadores = []
-#   lista_de_compras = []
 
     while True:
         print("""
@@ -48,36 +40,13 @@ def menu():
             lista_de_eventos.append(nova_evento)
         elif op == "le":
             imprime_lista_de_eventos(lista_de_eventos)
-        # elif op == "un":
-        #     novo_utilizador = cria_novo_utilizador()
-        #     lista_de_utilizadores.append(novo_utilizador)
-        # elif op == "ul":
-        #    imprime_lista_de_utilizadores(lista_de_utilizadores)
         elif op == "g":
-            guarda_as_listas_em_ficheiros(lista_de_eventos, lista_de_utilizadores)
+            guarda_as_listas_em_ficheiros(lista_de_eventos)
         elif op == "c":
             lista_de_eventos = carrega_as_listas_dos_ficheiros()
-        # elif op == "an":
-        #     if lista_de_utilizadores and lista_de_eventos:
-        #         id_comprador = pergunta_id(questao="Qual o id do comprador?", lista=lista_de_utilizadores)
-        #         id_evento = pergunta_id(questao="Qual o id do evento?", lista=lista_de_eventos)
-        #         lista_de_compras.append([id_comprador, id_evento, time.time()])
-        #     else:
-        #         print("Erro: tem de ter utilizadores e eventos")
-        # elif op == "al":
-        #     pass
-        #     # todo
-        #     # imprime_lista_de_compras()
 
 
 def pergunta_id(questao, lista):
-    """ ... ??to do??
-
-    :param questao:
-    :param lista:
-    :return:
-    """
-
     imprime_lista_de_dicionarios(lista)
     while True:
         idx = int(input(questao))
@@ -91,15 +60,12 @@ def carrega_as_listas_dos_ficheiros():
     """ ...todo... """
 
     lista_de_evento = le_de_ficheiro(nome_ficheiro_lista_de_eventos)
-    # lista_de_utilizadores = le_de_ficheiro(nome_ficheiro_lista_de_utilizadores)
     return lista_de_evento
-    # lista_de_utilizadores
 
 
 def guarda_as_listas_em_ficheiros(lista_de_evento):
     """ ... todo ....
 
-    :param lista_de_utilizadores:
     :param lista_de_evento:
     :return:
     """
@@ -107,7 +73,6 @@ def guarda_as_listas_em_ficheiros(lista_de_evento):
     op = input("Os dados nos ficheiros serão sobrepostos. Continuar (S/n)?")
     if op in ['s', 'S', '']:
         guarda_em_ficheiro(nome_ficheiro_lista_de_eventos, lista_de_evento)
-    #    guarda_em_ficheiro(nome_ficheiro_lista_de_utilizadores, lista_de_utilizadores)
     else:
         print("Cancelada.")
 
