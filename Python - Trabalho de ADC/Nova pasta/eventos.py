6248bace7f050eeb667ffd2b38335e83f436b5f8
marcelo-m7 <117441129+marcelo-m7@users.noreply.github.com>
2022-12-14 13:15:14 +0000

    Removido os comentários, adicionado novo parâmetro (data do evento)

---
+++ Python - Trabalho de ADC/Nova pasta/eventos.py
@@ -0,0 +1,18 @@
+from io_terminal import imprime_lista
+
+nome_ficheiro_lista_de_eventos = "lista_de_eventos.pk"
+
+
+def cria_evento():
+    nome_evento = input("Nome do evento ")
+    artista = input("Artista ")
+    sala = input("Sala ").upper()
+    pvp = float(input("PVP do bilhete "))
+#    data_inicio = input("Data de início (DD-MM-AAA)? ")
+#    data_fim = input("Data de fim (DD-MM-AAA)? ")
+
+    return {"Artista ": artista, "Sala ": sala, "Nome do evento: ": nome_evento, "PVP do bilhete": pvp}
+
+
+def imprime_lista_de_eventos(lista_de_eventos):
+    imprime_lista(cabecalho="Lista de Eventos", lista=lista_de_eventos)
